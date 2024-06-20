50fef4fcb4940cbe76dcec4a36b5b57764e8c758
lbrecic <luka.brecic3@gmail.com>
2024-06-20 11:00:58 +0000

    feat: move flags validation to process cmd args
    Signed-off-by: lbrecic <luka.brecic3@gmail.com>

--- pkg/cmd/workspace/create.go
+++ pkg/cmd/workspace/create.go
@@ -109,29 +109,6 @@ var CreateCmd = &cobra.Command{
 			}
 			visited[*projects[i].Source.Repository.Url] = true
 			projects[i].EnvVars = getEnvVariables(&projects[i], profileData)
-
-			if builderFlag == create.DEVCONTAINER || devcontainerPathFlag != "" {
-				projects[i].Build = &apiclient.ProjectBuild{
-					Devcontainer: &apiclient.ProjectBuildDevcontainer{},
-				}
-				if devcontainerPathFlag != "" {
-					projects[i].Build.Devcontainer.DevContainerFilePath = &devcontainerPathFlag
-				}
-			}
-
-			if builderFlag == create.AUTOMATIC {
-				projects[i].Build = &apiclient.ProjectBuild{}
-			}
-
-			if builderFlag == create.NONE {
-				projects[i].Build = nil
-			}
-
-			if customImageFlag != "" || customImageUserFlag != "" {
-				projects[i].Build = nil
-				projects[i].Image = &customImageFlag
-				projects[i].User = &customImageUserFlag
-			}
 		}
 
 		target, err := getTarget(activeProfile.Name)
@@ -327,6 +304,21 @@ func processCmdArguments(args []string, apiClient *apiclient.APIClient, projects
 			Repository: repoResponse,
 		},
 		Build: &apiclient.ProjectBuild{},
+	}
+
+	if builderFlag == create.DEVCONTAINER || devcontainerPathFlag != "" {
+		project.Build.Devcontainer = &apiclient.ProjectBuildDevcontainer{}
+		if devcontainerPathFlag != "" {
+			project.Build.Devcontainer.DevContainerFilePath = &devcontainerPathFlag
+		}
+	}
+
+	if builderFlag == create.NONE || customImageFlag != "" || customImageUserFlag != "" {
+		project.Build = nil
+		if customImageFlag != "" || customImageUserFlag != "" {
+			project.Image = &customImageFlag
+			project.User = &customImageUserFlag
+		}
 	}
 
 	*projects = append(*projects, *project)
